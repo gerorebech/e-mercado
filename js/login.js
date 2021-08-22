be63e446f6a0b6573b4df9cf27a970c831e74993
gerorebech <88357936+gerorebech@users.noreply.github.com>
2021-08-22 12:22:48 -0300

    Cambios

--- js/login.js
+++ js/login.js
@@ -1,27 +1,28 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
-    document.getElementById("logbttn").addEventListener("click", function () {
-        let inputEmail = document.getElementById("usuario");
-        let inputPassword = document.getElementById("password");
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("submitBtn").addEventListener("click", function () {
+        let inputEmail = document.getElementById("imputEmail");
+        let inputPassword = document.getElementById("imputPassword");
         let camposCompletos = true;
+        //console.log("Hola")
         if (inputEmail.value === '') {
-            inputEmail.classList.add("invalid");
             camposCompletos = false;
+            inputEmail.classList.add("invalid")
         } else {
             inputEmail.classList.remove("invalid");                     //CONTROL DE VALIDACIÓN!!!!
-                                                                        //CONTROL DE VALIDACIÓN!!!!
         }
         if (inputPassword.value === '') {
-            inputPassword.classList.add("invalid");
             camposCompletos = false;
+            inputPassword.classList.add("invalid");
         } else {
             inputPassword.classList.remove("invalid");
         }
         if (camposCompletos) {
-            window.location = "inicio.html";
+            window.location = "index.html";
         } else {
             alert("Ingresar los datos por favor");
         }
