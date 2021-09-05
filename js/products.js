6b4693da62a2406dbbee3a64b12a5f8097de55b9
gerorebech <88357936+gerorebech@users.noreply.github.com>
2021-09-05 09:40:29 -0300

    05/09/2021

--- js/products.js
+++ js/products.js
@@ -38,25 +38,25 @@ function muestroproductos(array) {
 function mostrar() {
     let precios = '';
 
-    for (let i = 0; i < precios.length; i++) {
-        let producto = producto[i]
+    for (let i = 0; i < productos.length; i++) {
+        let producto = productos[i]
 
         if (producto.precio.toLowerCase().indexOf(buscar) != -1) {
 
-            producto += `
+            precios += `
             <a href="category-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src="` + articulo.imgSrc + `" alt="` + articulo.description + `" class="img-thumbnail">
+                        <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ articulo.name + `</h4>
-                            <small class="text-muted">` + articulo.soldCount + ` artículos</small>
+                            <h4 class="mb-1">`+ producto.name + `</h4>
+                            <small class="text-muted">` + producto.soldCount + ` artículos</small>
                         </div>
-                        <p class="mb-1" >` + articulo.description + `</p>
+                        <p class="mb-1" >` + producto.description + `</p>
                         <br><br><br>
-                        <p class="mb-1">`+ "Precio" + " " + articulo.currency + articulo.cost + `</p>
+                        <p class="mb-1">`+ "Precio" + " " + producto.currency + producto.cost + `</p>
                         
                     </div>
                 </div>
@@ -79,8 +79,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
     })
 
     document.getElementById("buscar").addEventListener('input', function () {
-        productos = document.getElementById("buscar").value.toLowerCase();
-        mostrar(PRODUCTS_URL)
+        buscar = document.getElementById("buscar").value.toLowerCase();
+        mostrar(productos)
     })
 });
 
